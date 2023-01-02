47edd53bdba9a19124005768d18d0f2e2295aed0
JOEL3-PC\Joel3-PC <croftjoel@gmail.com>
2023-01-02 16:10:37 +0000

    Initial commit - SignalR Setup

---
+++ Scripts/SingalR.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("It Worked");
+            $.connection.room.server.announce("howdy");
+        })
+        .fail(function () { alert("fail"); });
+
+    $.connection.room.client.announce = function (message) {
+        $("#chatLog").append(message + "<br />");
+    };
+})();
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatWebApp.Startup))]
+namespace ChatWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
