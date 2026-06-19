64dbae99ba727d3e3cd0f4769a34371755ae10e7
NiobiumFire <CROFTJOEL@GMAIL.COM>
2026-06-19 23:08:00 +0100

    Allow local debug and test from phone when running directly in VS

--- Program.cs
+++ Program.cs
@@ -71,9 +71,9 @@ internal class Program
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
+            app.UseHttpsRedirection();
         }
 
-        app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
